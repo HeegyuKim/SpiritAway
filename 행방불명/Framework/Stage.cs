a8f29f2c39d7a858c685f8f0f62fdb82afcb6c33
Heegyu Kim <khk6435@naver.com>
2014-10-21 20:35:06 +0900

    asdf

--- 행방불명/Framework/Stage.cs
+++ 행방불명/Framework/Stage.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace 행방불명.Framework
 {
-
+	// 
+	// 모든 스테이지들이 상속받는
+	// 스테이지 인터페이스
+	//
 	public interface Stage
 	{
+
 		void Update(float delta);
+		
 		void Draw(Program program);
+		
 		Stage getNextStage();
-		bool IsEnd();
+
+		bool IsEnded();
 	}
 }
--- 행방불명/Program.cs
+++ 행방불명/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using 행방불명.Framework;
+using 행방불명.Game;
 using SharpDX.Toolkit;
 using System.Windows.Forms;
 using System.Drawing;
@@ -11,52 +12,77 @@ using SharpDX.DXGI;
 
 namespace 행방불명
 {
-	public class Program : Form
+	/*
+	 * Program 클래스는 Main 정적 메서드가 구현되어 있고
+	 * 그래픽 객체들을 갖고 있고
+	 * 스테이지들을 담당하고
+	 * FPS를 관리함.
+	 */
+	public class Program
 	{
 		private Graphics2D g2d;
 		private Graphics3D g3d;
+		private Form mForm;
 
-		SharpDX.Direct2D1.Bitmap bitmap;
 
 		public Graphics2D Graphics2D { get { return g2d; } }
 		public Graphics3D Graphics3D { get { return g3d; } }
+		public Form Form { get { return mForm; } }
+
 
 		public Program()
 		{
-			Text = "행방불명";
-			ClientSize = new Size(800, 600);
-			MaximizeBox = false;
-			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			mForm = new Form();
+			mForm.Text = "행방불명";
+			mForm.ClientSize = new Size(800, 600);
+			mForm.MaximizeBox = false;
+			mForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 
-			g3d = new Graphics3D(Handle, ClientSize.Width, ClientSize.Height, false);
+			g3d = new Graphics3D(mForm.Handle, mForm.ClientSize.Width, mForm.ClientSize.Height, false);
 			g2d = new Graphics2D(g3d.SwapChain.GetBackBuffer<Surface>(0));
 
-			bitmap = g2d.LoadBitmap("pic_test.png");
+			mCurrStage = new StartStage();
 
-			Paint += OnDraw;
+			mTimer = new Timer();
+			mTimer.Interval = 16;
+			mTimer.Tick += OnUpdate;
+			mTimer.Start();
 		}
-
-		private void OnDraw(object sender, PaintEventArgs args)
+		
+		~Program()
 		{
-			var rt = g2d.RenderTarget;
-			rt.BeginDraw();
-			rt.Clear(new SharpDX.Color4(1, 0.5f, 0.5f, 1));
-			rt.DrawBitmap(
-				bitmap, 
-				new SharpDX.RectangleF(0, 0, 500, 700),
-				1.0f, 
-				SharpDX.Direct2D1.BitmapInterpolationMode.Linear
-				);
-			rt.EndDraw();
-
-			g3d.SwapChain.Present(0, PresentFlags.None);
+			if(mTimer.Enabled)
+				mTimer.Stop();
 		}
 
+		private Stage mCurrStage;
+		private Timer mTimer;
+
+		private void OnUpdate(object sender, EventArgs args)
+		{
+			var stage = mCurrStage;
+
+			stage.Update(16.6f);
+			stage.Draw(this);
+
+			if (stage.IsEnded())
+			{
+				mCurrStage = stage.getNextStage();
+				if (mCurrStage == null)
+					mTimer.Stop();
+
+				var obj = stage as IDisposable;
+				if (obj != null)
+					obj.Dispose();
+			}
+		}
+		
+		
 
 		[STAThread]
 		public static void Main(string []args) 
 		{
-			Application.Run(new Program());
+			Application.Run(new Program().Form);
 		}
 	}
 }
